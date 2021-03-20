30b10e453e4bc5fd08e0286aa31ddfecc3336f81
prithupareek <prithu.pareek2019@gmail.com>
2021-03-20 01:07:34 -0400

    Made OOP + added interactive nonogram board

--- src/main.py
+++ src/main.py
@@ -1,31 +1,90 @@
 # import the pygame module, so you can use it
 import pygame as pg
+from pygame import mouse
+
+# some constants
+EMPTY = 0
+SELCTED = 1
+CROSSED = 2
 
 class Nonogram(object):
-    def __init__(self) -> None:
+    def __init__(self, screen) -> None:
         self.rows = 10
         self.cols = 10
+
+        self._screen = screen
+        self._width, self._height = self._screen.get_size()
 
         self._cellDims = 50
         self._gridColor = (0, 0, 0)
 
-    def draw(self, screen):
-        # get the dimentions of the screen
-        screenW, screenH = screen.get_size()
+        self._crossImg = pg.image.load('assets/cross.png')
+        self._crossImg = pg.transform.scale(self._crossImg, (self._cellDims - 10, self._cellDims - 10))
 
+        self.gameState = [[0 for col in range(self.cols)] for row in range(self.rows)]
+        # self.prettyPrintGameState()
+
+    def prettyPrintGameState(self):
+        for row in range(self.rows):
+            for col in range(self.cols):
+                print(self.gameState[row][col], end=" ")
+            print('')
+        print('')
+
+    def getCellFromPos(self, pos):
+        mouseX, mouseY = pos
+
+        # calculate the starting location for the grid
+        outerW = self.cols * self._cellDims
+        outerH = self.rows * self._cellDims
+        outerX = self._width/2 - outerW/2 + (3/2) * self._cellDims
+        outerY = self._height/2 - outerH/2 + (3/2) * self._cellDims
+
+        # very ineffeficient, but should get the job done
+        # loop through all cells and see if mouse pos is in bounds
+        for row in range(self.rows):
+            for col in range(self.cols):
+                xMin = outerX + col * self._cellDims
+                xMax = xMin + self._cellDims
+                yMin = outerY + row * self._cellDims
+                yMax = yMin + self._cellDims
+
+                if xMin < mouseX < xMax and yMin < mouseY < yMax:
+                    return (row, col)
+        
+        # No cell found
+        return (-1, -1)
+
+    def mousePressed(self):
+        # get the position of the mouse click
+        pos = pg.mouse.get_pos()
+
+        # get the cell clicked
+        row, col = self.getCellFromPos(pos)
+
+        # if clicked in grid
+        if (row, col) != (-1, -1):
+            # modify the game state appropriately
+            self.gameState[row][col] = (self.gameState[row][col] + 1) % 3
+
+            # self.prettyPrintGameState()
+
+
+
+    def draw(self):
         # Draw the outer rectangle 
         outerW = self.cols * self._cellDims
         outerH = self.rows * self._cellDims
-        outerX = screenW/2 - outerW/2 + (3/2) * self._cellDims
-        outerY = screenH/2 - outerH/2 + (3/2) * self._cellDims
-        pg.draw.rect(screen, self._gridColor, pg.Rect(outerX, outerY, outerW, outerH),  2) 
+        outerX = self._width/2 - outerW/2 + (3/2) * self._cellDims
+        outerY = self._height/2 - outerH/2 + (3/2) * self._cellDims
+        pg.draw.rect(self._screen, self._gridColor, pg.Rect(outerX, outerY, outerW, outerH),  2) 
 
         # Draw the grid
         for row in range(self.rows):
             for col in range(self.cols):
                 x = outerX + col * self._cellDims
                 y = outerY + row * self._cellDims
-                pg.draw.rect(screen, self._gridColor, pg.Rect(x, y, self._cellDims, self._cellDims),  2) 
+                pg.draw.rect(self._screen, self._gridColor, pg.Rect(x, y, self._cellDims, self._cellDims),  2) 
 
         # draw the side wings of the board
         for row in range(self.rows):
@@ -33,7 +92,7 @@ class Nonogram(object):
             y = outerY + row * self._cellDims
             w = self._cellDims * 3
             h = self._cellDims
-            pg.draw.rect(screen, self._gridColor, pg.Rect(x, y, w, h),  2) 
+            pg.draw.rect(self._screen, self._gridColor, pg.Rect(x, y, w, h),  2) 
 
         # draw the top wings of the board
         for col in range(self.cols):
@@ -41,7 +100,20 @@ class Nonogram(object):
             x = outerX + col * self._cellDims
             h = self._cellDims * 3
             w = self._cellDims
-            pg.draw.rect(screen, self._gridColor, pg.Rect(x, y, w, h),  2) 
+            pg.draw.rect(self._screen, self._gridColor, pg.Rect(x, y, w, h),  2) 
+
+        # draw the state of each cell
+        for row in range(self.rows):
+            for col in range(self.cols):
+                x = outerX + col * self._cellDims
+                y = outerY + row * self._cellDims
+
+                if self.gameState[row][col] == SELCTED:
+                    pg.draw.rect(self._screen, self._gridColor, pg.Rect(x + 5, y + 5, self._cellDims - 10, self._cellDims - 10)) 
+                elif self.gameState[row][col] == CROSSED:                    
+                    self._screen.blit(self._crossImg, (x + 5,y + 5))
+                    
+
 
     def update(self, keys):
         pass
@@ -60,25 +132,27 @@ class Game(object):
 
         self._keys = pg.key.get_pressed()
 
-        self._nonogram = Nonogram()
+        self._nonogram = Nonogram(self._screen)
 
-    def event_loop(self):
+    def eventLoop(self):
         for event in pg.event.get():
             self._keys = pg.key.get_pressed()
             if event.type == pg.QUIT or self._keys[pg.K_ESCAPE]:
                 self._running = False
+            if event.type == pg.MOUSEBUTTONUP:
+                self._nonogram.mousePressed()
 
     def draw(self):
         self._screen.fill(pg.Color("white"))
-        self._nonogram.draw(screen=self._screen)
+        self._nonogram.draw()
 
     def update(self):
         self._nonogram.update(keys=self._keys)
 
-    def main_loop(self) -> None:
+    def mainLoop(self) -> None:
         # main loop
         while self._running:
-            self.event_loop()
+            self.eventLoop()
             self.update()
             self.draw()
             pg.display.update()
@@ -86,7 +160,7 @@ class Game(object):
 # define a main function
 def main():
      game = Game()
-     game.main_loop()
+     game.mainLoop()
      
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
