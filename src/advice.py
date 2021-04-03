f43e633433b4312fa1618fe8db395dc0fe29e921
Ishraaq <ishraaqsshams@gmail.com>
2021-04-03 16:46:47 -0400

    start advice

--- src/advice.py
+++ src/advice.py
@@ -1,2 +1,21 @@
+NUMBER_EQ_GRIDSIZE = 0
+OVERLAPPING = 1
+NUM_SEP_BY_1 = 2
+UNREACHABLE = 3
+STILL_SPACE = 4
+MAKE_COMPLETE = 5
+MERGE_AND_SPLIT = 6
+DISTINGUISH_COMPLETE_GROUPS = 7
+CONTRADICTION = 8
 
+def check_num_eq_gridsize(gameState):
+    length = len(gameState)
+    for i in range(length):
+        
 
+# return list of advice that can be given given game state
+def list_of_advice():
+    # check each advice and append it to the return list
+    advice = list()
+    if (check_num_eq_gridsize(gameState)):
+        advice.append(NUMBER_EQ_GRIDSIZE)
